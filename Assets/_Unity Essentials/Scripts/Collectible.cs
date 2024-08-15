5da87b00d1527a387575d4e503cf82c37ab84715
AragaoMV <marcosvinisa@icloud.com>
2024-08-15 18:44:38 -0300

    Collectible just interact with the player

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -10,21 +10,27 @@ public class Collectible : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,rotationSpeed,0);
+        transform.Rotate(0, rotationSpeed, 0);
     }
 
-   
-    private void OnTriggerEnter(Collider other) {
-         //Destroy the collectible
-        Destroy(gameObject);
 
-        //Iniciate Particle VFX
-        Instantiate(onCollectEffect, transform.position, transform.rotation);
+    private void OnTriggerEnter(Collider other)
+    {
+            //Collectible item just interact with the player
+        if (other.CompareTag("Player"))
+        {
+            //Destroy the collectible
+            Destroy(gameObject);
+
+            //Iniciate Particle VFX
+            Instantiate(onCollectEffect, transform.position, transform.rotation);
+        }
+
     }
 }
