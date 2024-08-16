8d0c8f813a1369a4461bc77a2d16a8b726704a33
AragaoMV <marcosvinisa@icloud.com>
2024-08-16 10:56:48 -0300

    Added Jump into Player

--- Assets/_Unity Essentials/Scripts/PlayerController.cs
+++ Assets/_Unity Essentials/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f; // Set player's movement speed.
     public float rotationSpeed = 120.0f; // Set player's rotation speed.
+    public float jumpForce = 5.0f; // Set Players Jump Force.
 
     private Rigidbody rb; // Reference to player's Rigidbody.
 
@@ -17,7 +18,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))//Check if jump button is pressed
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        }
     }
 
 
